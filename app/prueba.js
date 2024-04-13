e704d9ed7e454ca4237b070dab76cfa520ca358a
Juan Paz <jpaz.dev94@gmail.com>
2024-04-13 15:11:04 -0300

    Actualizando api-football

--- app/prueba.js
+++ app/prueba.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require('axios')
 const path = require('node:path')
 const fs = require('node:fs/promises')
 
@@ -11,7 +11,7 @@ const config = {
     'x-rapidapi-key': API_KEY,
     'x-rapidapi-host': 'v3.football.api-sports.io'
   }
-};
+}
 
 // axios(config)
 // .then(function (response) {
@@ -26,25 +26,25 @@ const config = {
 //     writeJSONStatus(JSON.stringify(response.data))
 // }
 
-async function writeJSONStatus(data){
-    await fs.writeFile('./fixture-primera-c-2024.json',data)
-    console.log('Se creo la data');
+async function writeJSONStatus (data) {
+  await fs.writeFile('./fixture-primera-c-2024.json', data)
+  console.log('Se creo la data')
 }
 
 // getStatus()
 
-async function getLigasArgentina(){
-    const config = {
-        method: 'get',
-        url: 'https://v3.football.api-sports.io/fixture?league=131&season=2024',
-        headers: {
-          'x-rapidapi-key': API_KEY,
-          'x-rapidapi-host': 'v3.football.api-sports.io'
-        }
-      };
-
-        const response = await axios(config)
-        writeJSONStatus(JSON.stringify(response.data))
+async function getLigasArgentina () {
+  const config = {
+    method: 'get',
+    url: 'https://v3.football.api-sports.io/fixture?league=131&season=2024',
+    headers: {
+      'x-rapidapi-key': API_KEY,
+      'x-rapidapi-host': 'v3.football.api-sports.io'
     }
+  }
 
-    getLigasArgentina()
+  const response = await axios(config)
+  writeJSONStatus(JSON.stringify(response.data))
+}
+
+getLigasArgentina()
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
-const {verPathDelArchivo} = require('./app/prueba')
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    console.log('Conexion con exito :)');
-    verPathDelArchivo()
+app.get('/', (req, res) => {
+  console.log('Conexion con exito :)')
 })
 
 app.listen(3000, () => {
   console.log('Server: http://localhost:3000')
-  setTimeout(()=>{console.log('Simulando tarea asincrona');},5000)
+  setTimeout(() => { console.log('Simulando tarea asincrona') }, 5000)
 })
