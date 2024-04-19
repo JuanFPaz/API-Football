5799718921bef43a329a875f71105c6b92cb643a
Juan Paz <jpaz.dev94@gmail.com>
2024-04-19 17:00:52 -0300

    Separe en 'Capas', por asi decirlo, la solicitud, y las funciones de proceso de los endpoints. Teniendo un codigo un poquito más ordenado, limpio y claro.

--- app/util/readJson.js
+++ app/util/readJson.js
@@ -1,150 +1,263 @@
 /* eslint-disable promise/param-names */
 /* eslint-disable no-unused-vars */
-const { log } = require('node:console')
+const { log, error } = require('node:console')
 const { readFile } = require('node:fs/promises')
 const { resolve, join } = require('node:path')
 
 const DATA_PATH = resolve(__dirname, '../data')
 
-function getLinksArgentina () {
-  const CURRENT_SEASON = join(DATA_PATH, 'argentina', 'argentina.json')
-  return readFile(CURRENT_SEASON, 'utf-8')
-    .then(data => {
-      return JSON.parse(data)
-    })
-    .then(({ response }) => {
-      // nombre a response como res, para evitar que se soobrescriban
-
-      const [,,,,, ligaProfesional, copaArgentina, trofeoCampeones,, superCopa,, copaDeLaLiga] = response
-      const list = [ligaProfesional, copaDeLaLiga, copaArgentina, superCopa, trofeoCampeones]
-      const listFormateada = list.map(l => {
-        const { league, seasons } = l
-
-        const seasonsFormateada = seasons.map(s => {
-          const { year, start, end, current } = s
-          return { year, start, end, current }
-        })
-        return { league, seasons: seasonsFormateada }
-      })
-      const data = {
-        country: {
-          name: 'Argentina',
-          code: 'AR',
-          flag: 'https://media.api-sports.io/football/leagues/128.png'
-        },
-        list: listFormateada
-      }
-      return data
-    })
-}
-function getLinksCONMEBOL () {
-  const CURRENT_SEASON = join(DATA_PATH, 'conmebol', 'conmebol.json')
-  return readFile(CURRENT_SEASON, 'utf-8')
-    .then(data => {
-      return JSON.parse(data)
-    })
-    .then(({ response }) => {
-      const list = [...response]
-      const listFormateada = list.map(l => {
-        const { league, seasons } = l
-
-        const seasonsFormateada = seasons.map(s => {
-          const { year, start, end, current } = s
-          return { year, start, end, current }
-        })
-        return { league, seasons: seasonsFormateada }
-      })
-      const data = {
-        country: {
-          name: 'CONMEBOL',
-          code: null,
-          flag: null
-        },
-        list: listFormateada
-      }
-      return data
-    })
-}
-function getLinksUEFA () {
-  const CURRENT_SEASON = join(DATA_PATH, 'uefa', 'uefa.json')
-  return readFile(CURRENT_SEASON, 'utf-8')
-    .then(data => {
-      return JSON.parse(data)
-    })
-    .then(({ response }) => {
-      const list = [...response]
-      const listFormateada = list.map(l => {
-        const { league, seasons } = l
-
-        const seasonsFormateada = seasons.map(s => {
-          const { year, start, end, current } = s
-          return { year, start, end, current }
-        })
-        return { league, seasons: seasonsFormateada }
-      })
-      const data = {
-        country: {
-          name: 'UEFA',
-          code: null,
-          flag: null
-        },
-        list: listFormateada
-      }
-      return data
-    })
-}
-
-async function getDataLeague (...params) {
-  const [country, season, nameLeague, nameData] = params
-  const nameDir = nameLeague
-  const nameFile = `${nameData}-${nameLeague}-${season}.json`
-  const DIR_PATH = join(DATA_PATH, country, 'season', season, nameDir, nameFile)
-  const data = await readFile(DIR_PATH)
-  const { response: [{ league: { standings } }] } = JSON.parse(data)
-  const standingsFormateadas = standings.map(standing => {
-    // Parece redundante pero no lo es, cada liga tiene una tabla, por ejemplo la copa de la liga es un arreglo con 2 tablas
-    // Seguramente la champions y la libertadores tengan 1 arreglo con 8 tablas
-    // y la liga es arreglo con 1 tabla
-    // Podria crear un standing.length === 1, evitar hacer el siguiente MAP, pero no veo porque no lo puede hacer
-    const standingFormateada = standing.map(equipo => {
-      const { team, points, goalsDiff, all, home, away } = equipo
-      return {
-        team,
-        points,
-        goalsDiff,
-        all,
-        home,
-        away
-      }
-    })
-    return standingFormateada
-  })
-
-  /* agrego nueva sentencia para reducir los standings en un solo arreglo, y hacer el every: */
-  const equipos = standingsFormateadas.reduce((acc, curr) => acc.concat(curr), [])
-  const partidosJugados = equipos.every(e => e.all.played === 0)
-  // const tablaInicial = standingFormateada.every(e => e.all.played === 0)
-
-  if (partidosJugados) {
-    const standingsSorteadas = standingsFormateadas.map(standing => {
-      return standing.sort((a, b) => (a.team.name > b.team.name) ? 1 : -1)
-    })
-    return [{ standings: standingsSorteadas }]
+async function processGetLinksArg () {
+  let dataPath
+  let dataFile
+  try {
+    dataPath = join(DATA_PATH, 'argentina', 'argentina.json')
+  } catch (err) {
+    const customError = {
+      process: 'getLinksArg',
+      message: 'Ocurrio un Error en process Get Links argentina, creando la ruta. - No creo que ocurra ningu nerror aca, por ahora.',
+      reference: 'EL CHIQUI TAPIAAA'
+    }
+    console.error(err)
+    throw customError
   }
 
-  // aca agregariamos el fixture, etx
-  return [{ standings: standingsFormateadas }]
+  try {
+    dataFile = await readFile(dataPath, 'utf-8')
+  } catch (err) {
+    const customError = {
+      process: 'getLinksArg',
+      message: 'Ocurrio un error leyendo el archivo.',
+      reference: err
+    }
+    console.error(customError.message)
+    throw customError
+  }
+
+  try {
+    const { response } = JSON.parse(dataFile)
+    const [,,,,, ligaProfesional, copaArgentina, trofeoCampeones,, superCopa,, copaDeLaLiga] = response
+    const list = [ligaProfesional, copaDeLaLiga, copaArgentina, superCopa, trofeoCampeones]
+    const listFormateada = list.map(l => {
+      const { league, seasons } = l
+
+      const seasonsFormateada = seasons.map(s => {
+        const { year, start, end, current } = s
+        return { year, start, end, current }
+      })
+      return { league, seasons: seasonsFormateada }
+    })
+    const data = {
+      country: {
+        name: 'Argentina',
+        code: 'AR',
+        flag: 'https://media.api-sports.io/football/leagues/128.png'
+      },
+      list: listFormateada
+    }
+    return data
+  } catch (err) {
+    const customError = {
+      process: 'getLinksCups',
+      message: 'Ocurrio un error procesando la respuesta, antes de envirla D:',
+      reference: err
+    }
+    console.error(customError.message)
+    throw customError
+  }
 }
 
-// Esto me pasa por no manejar los erroers como corresponde -_- ' !
-// getDataLeague('argentina', '2024', 'liga-profesional-argentina', 'standings') // Error undefined: 27 SyntaxError: Expected double-quoted property name in JSON at position 845 -.-
+async function processGetLinksCups (confederacion) {
+  let dataPath
+  let dataFile
 
-function getLinksPrincipal () {
-  return Promise.all([
-    getLinksArgentina(),
-    getLinksCONMEBOL(),
-    getLinksUEFA()
-  ])
+  try {
+    dataPath = join(DATA_PATH, confederacion, `${confederacion}.json`)
+  } catch (err) {
+    const customError = {
+      process: 'GetLinksCups',
+      message: 'Ocurrio un Error en process Get Links de la ÜEFA o CORRUPTBOL, creando la ruta. - No creo que ocurra ningu nerror aca, por ahora.',
+      reference: 'EL CHIQUI TAPIAAA aca tmb tiene la culpa'
+    }
+    console.error(err)
+    throw customError
+  }
+
+  try {
+    dataFile = await readFile(dataPath, 'utf-8')
+  } catch (err) {
+    const customError = {
+      process: 'GetLinksCups',
+      message: 'Ocurrio un error leyendo el archivo.',
+      reference: err
+    }
+    console.error(customError.message)
+    throw customError
+  }
+
+  try {
+    const { response } = JSON.parse(dataFile)
+    const list = response
+    const listFormateada = list.map(l => {
+      const { league, seasons } = l
+
+      const seasonsFormateada = seasons.map(s => {
+        const { year, start, end, current } = s
+        return { year, start, end, current }
+      })
+      return { league, seasons: seasonsFormateada }
+    })
+    const data = {
+      country: {
+        name: confederacion.toUpperCase(),
+        code: confederacion.toUpperCase(),
+        flag: null
+      },
+      list: listFormateada
+    }
+    return data
+  } catch (err) {
+    const customError = {
+      process: 'getLinksCups',
+      message: 'Ocurrio un error procesando la respuesta, antes de envirla D:',
+      reference: err
+    }
+    console.error(customError.message)
+    throw customError
+  }
+}
+
+async function processGetStanding (...params) {
+  /* Primer Proceso, crear la ruta de la solicitud: */
+  let dataPath
+  let dataFile
+  console.log('Creando ruta del archivo')
+  try {
+    if (params.length < 4) {
+      const customError = {
+        referenceCustomError: 'Se esperaban 4 argumentos, y solo pasamos ' + params.length
+      }
+
+      throw customError
+    }
+    const [country, season, nameLeague, nameData] = params
+    const nameLeagueFormated = nameLeague.toLowerCase().replace(/\s/g, '-') // Sirve para el nombre del directorio que queremos ir y el nmombre del archivo
+    const nameFile = `${nameData}-${nameLeagueFormated}-${season}.json`
+    // Ejemplo data path: '/data/argentina/season/2024/liga-profesional-argentina/standings-liga-profesional-argentina-2024.json'
+    dataPath = join(DATA_PATH, country, 'season', season, nameLeagueFormated, nameFile)
+  } catch (err) {
+    const customError = {
+      process: 'getStanding',
+      message: 'Ocurrio un Error en process standing, creando la ruta:',
+      reference: err
+    }
+    console.log(customError.message)
+    throw customError
+  }
+
+  console.log('Leyendo el jodido archivo')
+
+  try {
+    dataFile = await readFile(dataPath, 'utf-8')
+  } catch (err) {
+    const customError = {
+      process: 'getStanding',
+      message: 'Ocurrio un error leyendo el archivo.',
+      reference: err
+    }
+    console.error(customError.message)
+    throw customError
+  }
+
+  console.log('Procesando la respuesta :D')
+
+  try {
+    const { response: [{ league: { standings } }] } = JSON.parse(dataFile)
+    const standingsFormateadas = standings.map(standing => {
+      // Parece redundante pero no lo es, cada liga tiene una tabla, por ejemplo la copa de la liga es un arreglo con 2 tablas
+      // Seguramente la champions y la libertadores tengan 1 arreglo con 8 tablas
+      // y la liga es arreglo con 1 tabla
+      // Podria crear un standing.length === 1, evitar hacer el siguiente MAP, pero no veo porque no lo puede hacer
+      const standingFormateada = standing.map(equipo => {
+        const { team, points, goalsDiff, all, home, away } = equipo
+        return {
+          team,
+          points,
+          goalsDiff,
+          all,
+          home,
+          away
+        }
+      })
+      return standingFormateada
+    })
+
+    /* agrego nueva sentencia para reducir los standings en un solo arreglo, y hacer el every: */
+    const equipos = standingsFormateadas.reduce((acc, curr) => acc.concat(curr), [])
+    const partidosJugados = equipos.every(e => e.all.played === 0)
+    // const tablaInicial = standingFormateada.every(e => e.all.played === 0)
+
+    if (partidosJugados) {
+      const standingsSorteadas = standingsFormateadas.map(standing => {
+        return standing.sort((a, b) => (a.team.name > b.team.name) ? 1 : -1)
+      })
+      return { standings: standingsSorteadas }
+    }
+
+    // aca agregariamos el fixture, etx
+    return { standings: standingsFormateadas }
+  } catch (err) {
+    const customError = {
+      process: 'getStanding',
+      message: 'Ocurrio un error procesando la respuesta, antes de envirla D:',
+      reference: err
+    }
+    console.error(customError.message)
+    throw customError
+  }
+}
+
+async function getDataLeague ({ country, season, nameLeague, nameData }) {
+  /* TO DO: Capturar un error
+   correctamente del promises all,
+   porque si algo sale mal en algrun proceso,
+   debemos capturarlo en el endpoint de la solicitud.
+   Independientemente de que 3 de 4 procesos hayan salido bien
+   no podemos devolver un procesos mal y 3 bien xd
+  */
+  let data
+  try {
+    data = await Promise.all([processGetStanding(country, season, nameLeague, nameData), { fixture: ['holi'] }])
+    console.log('Todos los datos fueron obtenidos con exito')
+  } catch (err) {
+    console.error('Retornamos un error intero :/')
+    return { error: err }
+  }
+
+  return data
+}
+
+async function getDataCup () {
+  // Solicitud para tener las datas de las copas
+}
+
+async function getDataMatch () {
+  // Solicitud para tener los datos de un partido en especifico solicitado por el usaurio
+  // Ej: Boca vs RiBer
+}
+
+async function getLinksPrincipal () {
+  let data
+  try {
+    data = await Promise.all([
+      processGetLinksArg(), // a, en el catch de processGetLinks me estan lanzando un error, tengo que capturarlo y mandarlo a mi catch
+      processGetLinksCups('conmebol'),
+      processGetLinksCups('uefa')
+    ])
+  } catch (err) {
+    console.error('Retornamos un error intero :/')
+    return { error: err }
+  }
+  return data
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -18,11 +18,18 @@ app.use(cors())
 
 */
 app.get('/', async (req, res) => {
+  /*
+    Aca podria crear un fakeBody para que cuando solicitemos la listas de Links,
+     nos devuelva siempre por defecto, las compoetencias actuales.
+  */
   const data = {}
   try {
     data.timestamp = Date.now()
     data.response = await getLinksPrincipal()
     res.json(data)
+    if (data.response.error) {
+      throw data
+    }
   } catch (err) {
     /* TODO: manejar errores de solicitud :D */
     res.status(500).json(err)
@@ -30,25 +37,47 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/2024/liga-profesional-argentina', async (req, res) => {
-  console.log('Hola')
+  /*
+    Simulamos un req.body, para más adelante reciclar una funcion de proceso de request
+    y verificar que los datos recibidos son correctos,
+    y enviarlos a getDataLeague.
+  */
+  const fakeBody = {
+    country: 'argentina',
+    season: '2024',
+    nameLeague: 'Liga Profesional Argentina',
+    nameData: 'standings'
+  }
   const data = {}
   try {
     data.timestamp = Date.now()
-    data.response = await getDataLeague('argentina', '2024', 'liga-profesional-argentina', 'standings')
+    data.response = await getDataLeague({ ...fakeBody })
+    if (data.response.error) {
+      throw data
+    }
     res.json(data)
   } catch (error) {
-
+    res.status(500).json(error)
   }
 })
 
 app.get('/2024/copa-de-la-liga-profesional', async (req, res) => {
+  const fakeBody = {
+    country: 'argentina',
+    season: '2024',
+    nameLeague: 'Liga Profesional Argentina',
+    nameData: 'standings'
+  }
   const data = {}
   try {
     data.timestamp = Date.now()
-    data.response = await getDataLeague('argentina', '2024', 'copa-de-la-liga-profesional', 'standings')
+    data.response = await getDataLeague({ ...fakeBody })
+    if (data.response.error) {
+      throw data
+    }
     res.json(data)
   } catch (error) {
-
+    res.status(500).json(error)
   }
 })
 
