c504851d473a159d03a561d0d3e43d87b1415064
Juan Paz <jpaz.dev94@gmail.com>
2024-04-20 11:34:25 -0300

    Creando endpoints para Lali, la sudacup, la champions, la europa league y la conference league

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { getLinksPrincipal, getDataLeague } = require('./app/util/readJson')
+const { getLinksPrincipal, getDataLeague, getDataCup } = require('./app/util/readJson')
 
 const PORT = process.env.PORT ?? 3000
 
@@ -45,8 +45,8 @@ app.get('/2024/liga-profesional-argentina', async (req, res) => {
   const fakeBody = {
     country: 'argentina',
     season: '2024',
-    nameLeague: 'Liga Profesional Argentina',
-    nameData: 'standings'
+    nameLeague: 'Liga Profesional Argentina'
+    // nameData: ['standings']
   }
   const data = {}
   try {
@@ -65,8 +65,8 @@ app.get('/2024/copa-de-la-liga-profesional', async (req, res) => {
   const fakeBody = {
     country: 'argentina',
     season: '2024',
-    nameLeague: 'Liga Profesional Argentina',
-    nameData: 'standings'
+    nameLeague: 'Copa de la liga Profesional',
+    nameData: ['standings']
   }
   const data = {}
   try {
@@ -81,16 +81,104 @@ app.get('/2024/copa-de-la-liga-profesional', async (req, res) => {
   }
 })
 
-app.get('/copa-argentina', async (req, res) => {
-
+app.get('/2024/conmebol-libertadores', async (req, res) => {
+  const fakeBody = {
+    country: 'conmebol',
+    season: '2024',
+    nameLeague: 'CONMEBOL Libertadores',
+    nameData: ['standings', 'fixtures']
+  }
+  const data = {}
+  try {
+    data.timestamp = Date.now()
+    data.response = await getDataCup({ ...fakeBody })
+    if (data.response.error) {
+      throw data
+    }
+    res.json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
-app.get('/supercopa', async (req, res) => {
-
+app.get('/2024/conmebol-sudamericana', async (req, res) => {
+  const fakeBody = {
+    country: 'conmebol',
+    season: '2024',
+    nameLeague: 'CONMEBOL sudamericana',
+    nameData: ['standings', 'fixtures']
+  }
+  const data = {}
+  try {
+    data.timestamp = Date.now()
+    data.response = await getDataCup({ ...fakeBody })
+    if (data.response.error) {
+      throw data
+    }
+    res.json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
-app.get('/trofeo-de-campeones', async (req, res) => {
+app.get('/2023/uefa-champions-league', async (req, res) => {
+  const fakeBody = {
+    country: 'uefa',
+    season: '2023',
+    nameLeague: 'UEFA Champions League',
+    nameData: ['standings', 'fixtures']
+  }
+  const data = {}
+  try {
+    data.timestamp = Date.now()
+    data.response = await getDataCup({ ...fakeBody })
+    if (data.response.error) {
+      throw data
+    }
+    res.json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
+app.get('/2023/uefa-europa-league', async (req, res) => {
+  const fakeBody = {
+    country: 'uefa',
+    season: '2023',
+    nameLeague: 'UEFA Europa League',
+    nameData: ['standings', 'fixtures']
+  }
+  const data = {}
+  try {
+    data.timestamp = Date.now()
+    data.response = await getDataCup({ ...fakeBody })
+    if (data.response.error) {
+      throw data
+    }
+    res.json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+app.get('/2023/uefa-europa-conference-league', async (req, res) => {
+  const fakeBody = {
+    country: 'uefa',
+    season: '2023',
+    nameLeague: 'UEFA Europa Conference League',
+    nameData: ['standings', 'fixtures']
+  }
+  const data = {}
+  try {
+    data.timestamp = Date.now()
+    data.response = await getDataCup({ ...fakeBody })
+    if (data.response.error) {
+      throw data
+    }
+    res.json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 app.listen(PORT, () => {
