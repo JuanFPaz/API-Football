396ba60694eb2dfbbfbcd8ac6f5e528d3721ceb6
Juan Paz <jpaz.dev94@gmail.com>
2024-04-29 19:53:38 -0300

    Creando primera funcion de proceso, para separar los fixtures por rondas y enviarselo al front :)

--- app/util/processRead/processFixtures.js
+++ app/util/processRead/processFixtures.js
@@ -55,20 +55,93 @@ async function processGetFixtures (...params) {
 
     const fixturesFormateados = fases.map((f, idx) => {
       const fixtureFiltrado = fixtures.filter((fx) => fx.league.round === f).map((fx) => {
-        const { fixture: { id, date, venue, status }, teams, goals, score } = fx
-        return { id, date, venue, status, teams, goals, score }
+        const { fixture: { id, date, venue, status }, league: { round }, teams, goals, score } = fx
+        return { round, id, date, venue, status, teams, goals, score }
       })
 
       return {
-        jornada: `Fecha ${idx + 1}`,
+        jornada: f,
+        partidosLength: fixtureFiltrado.length,
+        // aca podria poner el status fixture global de los partidos
+        // Si no estan definidos todas los partidos, ponemos la unica fecha por defecto,
+        // De lo contrario, podemos filtrar los dias que se van a jugar, y devolverlo en u narreglo
+        // Para que el front pueda separar los partidos por dia y simplmente se encargue de ordenarlos por horarios.
         partidos: fixtureFiltrado
       }
     })
 
-    return { fixtures: fixturesFormateados }
+    const fixturePorRondas = rondasCopaDeLaLiga([...fixturesFormateados])
+    return { fixturesLength: fixturePorRondas.length, fixtures: fixturePorRondas }
   } catch (error) {}
 }
 
 module.exports = {
   processGetFixtures
 }
+
+function rondasCopaDeLaLiga (unFixture) {
+  const regex = /^1st Phase - (?:[1-9]|1[0-4])$/
+
+  const primeraFaseFormateada = unFixture.filter(f => regex.test(f.jornada))
+  const segundaFaseFormateada = unFixture.filter(f => !regex.test(f.jornada))
+
+  const primeraFase = [
+    {
+      roundName: 'Fase de grupos',
+      roundLength: primeraFaseFormateada.length,
+      rounds: primeraFaseFormateada
+    }
+  ]
+
+  const segundaFase = [
+    {
+      roundName: 'Fase Final',
+      roundLength: segundaFaseFormateada.length,
+      rounds: segundaFaseFormateada
+    }
+  ]
+  return [primeraFase, segundaFase]
+}
+
+// const asd = {
+fixtures: [
+  [
+    {
+      round: 'Fase de grupos',
+      fixtureLength: 14,
+      fixture: [
+        {
+          jornada: '1st Phase - 1',
+          partidosLength: 14,
+          partidos: [{ partido: 'Uno' }, { partido: 'dos' }/* etc */]
+        },
+        {
+          jornada: '1st Phase - 2',
+          partidosLength: 14,
+          partidos: [{ partido: 'Uno' }, { partido: 'dos' }/* etc */]
+        }
+        /* Asi hasta obtener todos los de 1st Phase  entre el 1 y el 14 */
+      ]
+    }
+  ],
+  [
+    {
+      round: 'Fase Final',
+      fixtureLength: 4,
+      fixture: [
+        {
+          jornada: '1st Phase - Cuartos de final',
+          partidosLength: 14,
+          partidos: [{ partido: 'Uno' }, { partido: 'dos' }/* etc */]
+        },
+        {
+          jornada: '1st Phase - Semifinal',
+          partidosLength: 14,
+          partidos: [{ partido: 'Uno' }, { partido: 'dos' }/* etc */]
+        }
+        /* Asi hasta obtener todos los que sean diferentes a 1st Phase  entre el 1 y el 14 */
+      ]
+    }
+  ]
+]
+// }
