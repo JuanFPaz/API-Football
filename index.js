014a188b078e898e8c9c236456ec9136a3850a08
Juan Paz <jpaz.dev94@gmail.com>
2024-07-18 17:16:14 -0300

    Actualizando los cambios en el archivo index.js - Creacion de middlewares

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { validateCreateLinks, validateCreateFxSt } = require('./app/middleware/validateCreate')
 const { validateReadLeague, validateReadLinks } = require('./app/middleware/validateRead')
+const { createLinks, createFixtures, createStandings } = require('./app/util/createJson')
 const { getDataLeague, getLinks } = require('./app/util/readJson')
+const bodyParse = require('./app/helpers/bodyParser')
 const pc = require('picocolors')
-// const { createLinks, createFixtures, createStandings } = require('./app/util/createJson')
 
 const PORT = process.env.PORT ?? 3000
 
@@ -14,10 +16,25 @@ app.use(cors())
 app.use((req, res, next) => {
   const { method, url } = req
   console.log(' ')
-  console.log(`${pc.bgCyan(method + ':')} ${pc.green(url)}`)
+  console.log(`${pc.bgCyan('URL :')} ${pc.green(url)}`)
+  console.log(`${pc.bgCyan('METHOD :')} ${pc.green(method)}`)
   console.log(`${pc.bgCyan('DATE:')} ${pc.green(new Date().toLocaleString())}`)
   next()
 })
+
+app.post('/:resource', bodyParse)
+
+app.post('/links', validateCreateLinks)
+
+app.post('/links', createLinks)
+
+app.post('/fixtures', validateCreateFxSt)
+
+app.post('/fixtures', createFixtures)
+
+app.post('/standings', validateCreateFxSt)
+
+app.post('/standings', createStandings)
 
 app.get('/', validateReadLinks)
 
@@ -29,23 +46,13 @@ app.get('/:country/:season/:league', getDataLeague)
 
 app.use((req, res) => {
   const data = {}
-  data.get = req.url
+  data.url = req.url
   data.timestamp = Date.now()
   data.error = { message: 'URL Inexistente' }
   console.log(`${pc.bgRed('Request Invalid')}`)
-  console.log(`${pc.bgRed('Message:')} ${pc.red(data.error)}`)
+  console.log(`${pc.bgRed('Message:')} ${pc.red(data.error.message)}`)
   res.status(400).json(data)
 })
-// app.use('/:season/:league', (req, res, next) => {
-//   console.log(req.params)
-//   const data = {
-//     league: req.params.league,
-//     season: req.params.season
-//   }
-//   req.cookies = JSON.stringify(data)
-//   next()
-// })
-
 // app.post('/links', async (req, res) => {
 //   const { method, url } = req
 
